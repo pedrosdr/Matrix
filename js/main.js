833979e3efc96f935bdf78b2e21dcba0e75c0f64
P7ori <pedrosartdr@gmail.com>
2023-05-19 18:48:47 -0300

    added method D to find the Minor of a Matrix

--- js/main.js
+++ js/main.js
@@ -56,6 +56,29 @@ class Matrix
             this._arr[row][j-1] = values[row]
         }
     }
+
+    D(i, j)
+    {
+        if(this._ncol !== this._nrow)
+            throw new MatrixError('Error geting D: Matrix must be square')
+
+        let n = this._nrow
+        let d = new Matrix(n-1, n-1)
+        let rowIndex = 1
+        for(let row in this._arr)
+        {
+            if(row == i - 1) continue
+            let dRow = Array()
+            for(let item in this._arr[row])
+            {
+                if(item == j - 1) continue
+                dRow.push(this._arr[row][item])
+            }
+            d.setRow(rowIndex, dRow)
+            rowIndex += 1
+        }
+        return d
+    }
 }
 
 class MatrixError extends Error 
@@ -67,14 +90,22 @@ class MatrixError extends Error
     }
 }
 
-let mat = new Matrix(5, 4)
+// let mat = new Matrix(5, 5)
 
-mat.setRow(1, [1, 2, 3, 0])
-mat.setRow(2, [4, 5, 6, 0])
-mat.setRow(3, [7, 8, 9, 0])
-mat.setRow(4, [1, 2, 3, 0])
-mat.setRow(5, [4, 5, 6, 0])
-mat.setCol(4, [4, 7, 1, 4, 7])
+// mat.setRow(1, [1, 2, 3, 4, 0])
+// mat.setRow(2, [4, 5, 6, 3, 0])
+// mat.setRow(3, [7, 8, 9, 2, 0])
+// mat.setRow(4, [1, 2, 3, 1, 0])
+// mat.setRow(5, [4, 5, 6, 1, 0])
+// mat.setCol(5, [4, 7, 1, 4, 7])
+// console.log(mat.D(1, 2))
+
+let mat = new Matrix(3, 3)
+mat.setRow(1, [2, 3, 1])
+mat.setRow(2, [1, 1, 5])
+mat.setRow(3, [4, 9, 7])
 console.log(mat)
+
+console.log(mat.D(2, 2))
 
 
